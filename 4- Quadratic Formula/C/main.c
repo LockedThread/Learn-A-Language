e9854bd2f580f1810f744ca579a9f477b880fee1
Warren Snipes <contact@warrensnipes.dev>
2021-03-17 22:43:36 -0400

    Finished Quadratic Formula in C

---
+++ 4- Quadratic Formula/C/main.c
@@ -0,0 +1,108 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <math.h>
+
+#define MAX_FUNCTION_SIZE 255
+
+void removeSpaces(char *s);
+
+void getQuadraticFunctionVariables(char *sourceString, double *a, double *b, double *c);
+
+double evaluateQuadraticFunction(int pos, double a, double b, double c);
+
+int main() {
+    printf("Quadratic Formula Calculator. Enter your Quadratic Function (0 to exit).\n");
+    do {
+        printf("\nEnter your Quadratic Function: ");
+
+        char *functionString = malloc(MAX_FUNCTION_SIZE + 1);
+
+        fgets(functionString, MAX_FUNCTION_SIZE, stdin);
+
+        // Removes trailing
+        unsigned long len = strlen(functionString);
+        if (len > 0 && functionString[len - 1] == '\n')
+            functionString[len - 1] = '\0';
+
+        printf("\n%s", functionString);
+
+        removeSpaces(functionString);
+
+        printf("\nResult: %s", functionString);
+        double a, b, c;
+        getQuadraticFunctionVariables(functionString, &a, &b, &c);
+        free(functionString);
+
+        printf("\na=%f, b=%f, c=%f", a, b, c);
+
+
+        double posRoot = evaluateQuadraticFunction(1, a, b, c), negRoot = evaluateQuadraticFunction(0, a, b, c);
+
+        printf("\nposRoot=%f, negRoot=%f", posRoot, negRoot);
+
+        break;
+    } while (1);
+}
+
+void removeSpaces(char *s) {
+    const char *d = s;
+    do {
+        while (*d == ' ') {
+            ++d;
+        }
+    } while ((*s++ = *d++));
+}
+
+double evaluateQuadraticFunction(int pos, double a, double b, double c) {
+    double sub = sqrt(b * b - 4 * (a * c));
+    return (pos ? -b + sub : -b - sub) / (2 * a);
+}
+
+void getQuadraticFunctionVariables(char *sourceString, double *a, double *b, double *c) {
+    printf("\nSource String: %s", sourceString);
+    char *copy = strdup(sourceString);
+    char *delim = "+-";
+    char *p = strtok(sourceString, delim);
+
+    int count = 0;
+    while (p != NULL) {
+        char delimUsed = copy[p - sourceString - 1];
+
+        char buffer[100];
+
+        int counter = 0;
+        if (delimUsed == '+' || delimUsed == '-') {
+            buffer[0] = delimUsed;
+            counter = 1;
+        }
+        for (int i = 0; i < strlen(p); i++) {
+            char current = p[i];
+            if (current != 'x') {
+
+                buffer[counter++] = current;
+            } else {
+                break;
+            }
+        }
+        double coefficient = strtod(buffer, NULL);
+        memset(buffer, 0, strlen(buffer));
+
+        switch (count) {
+            case 0:
+                *a = coefficient;
+                break;
+            case 1:
+                *b = coefficient;
+                break;
+            case 2:
+                *c = coefficient;
+                break;
+            default:
+                // TODO: idk
+                break;
+        }
+        count++;
+        p = strtok(NULL, "+-");
+    }
+}
